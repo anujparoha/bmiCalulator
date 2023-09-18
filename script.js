df45d4cd8bd492428fe2d86182578bf7d772e8c7
anuj paroha <109169449+anujparoha@users.noreply.github.com>
2023-09-18 15:52:01 +0530

    Update script.js
    add new evengt

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const form = document.querySelector('form');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+const heightInput = document.querySelector('#height');
+const weightInput = document.querySelector('#weight');
+const result = document.querySelector('#result');
 
-    const height = parseInt(document.querySelector('#height').value);
-    const weight = parseInt(document.querySelector('#weight').value);
-    const result = document.querySelector('#result');
-    
-    if (height === '' || height < 0 || isNaN(height)) {
-        result.innerHTML = "OOPS!! 😁 Please provide a valid Height";
-    } else if (weight === '' || weight < 0 || isNaN(weight)) {
-        result.innerHTML = "OOPS!! 😁 Please provide a valid Weight";
+function calculateBMI() {
+    const height = parseInt(heightInput.value);
+    const weight = parseInt(weightInput.value);
+
+    if (isNaN(height) || isNaN(weight) || height === '' || weight === '') {
+        result.innerHTML = " 😵‍💫😵‍💫 Please provide both height and weight values";
+    } else if (height < 0 || weight < 0) {
+        result.innerHTML = "OOPS!! 😁 Height and weight values should be positive";
     } else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         if (bmi < 18.6) {
             result.innerHTML = `<span>Your result is ${bmi}<br> You Need to gain weight 🫠🫠</span>`;
-        } else if (bmi > 18.6 && bmi < 24.9) {
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
             result.innerHTML = `<span>Your result is ${bmi}<br> Congrats 😎😎 You are in the healthy range</span>`;
         } else {
             result.innerHTML = `<span>Your result is ${bmi}<br> Oops 🥲🥲! You are Overweight</span>`;
         }
+    }
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    calculateBMI();
+});
+
+heightInput.addEventListener('keyup', function (e) {
+    if (e.key === 'Enter') {
+        calculateBMI();
+    }
+});
+
+weightInput.addEventListener('keyup', function (e) {
+    if (e.key === 'Enter') {
+        calculateBMI();
     }
 });
